2f899da68b463919806454ac7eecc5b64009f9c0
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-10 08:50:16 +1100

    insertAfter implemented

--- linkedList.cpp
+++ linkedList.cpp
@@ -31,12 +31,29 @@ void printLL(Node* head) {
     cout << endl;
 }
 
+void insertAfter(int data, int data_2,  Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        if (current->data == data) {
+            Node* n = new Node(data_2);
+            n->next = current->next;
+            current->next = n;
+            return;
+        }
+        else {
+            current = current->next;
+        }
+    }
+
+}
+
 int main(void) {
     Node* head = new Node(5);
     insertAtHead(head, 4);
     insertAtHead(head, 3);
     insertAtHead(head, 2);
     insertAtHead(head, 1);
+    insertAfter(2, 10, head);
     printLL(head);
 
     return 0;
